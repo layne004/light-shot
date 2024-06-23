e2318a03f6660691a2c2b4ea3232235562ed6d7c
2022051615189dengke <xxx@xxx>
2024-06-23 23:29:44 +0800

    commit 1:获取winid。

--- light-shot/windowundercursor.cpp
+++ light-shot/windowundercursor.cpp
@@ -7,11 +7,11 @@ QImage WindowUnderCursor::captureWindowUnderCursor()
     QPoint cursorPos = QCursor::pos();
     QWindow *window = QApplication::windowAt(cursorPos);
     if (!window)
-        return QImage(); // No window found
+        return QImage();
 
     QScreen *screen = window->screen();
     if (!screen)
-        return QImage(); // No screen found
+        return QImage();
 
     return screen->grabWindow(window->winId()).toImage();
 }
